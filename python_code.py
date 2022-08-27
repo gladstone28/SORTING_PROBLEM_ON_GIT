310892103e1f2d679cf31b258a8a33ec8dd05e28
Gladstone Rose <gladstonerose@hotmail.com>
2022-08-27 01:06:35 -0500

    added a .gitignore file to prevent .txt and .pd files to go to this Repo. Also transferred thiumb drive files to the project folder.

--- python_code.py
+++ python_code.py
@@ -1,2 +1,3 @@
 print("This is Gladstone setting up a Python Code in nano")
 print("This is another print statement in my Python code which I am using to test my understanding of git/github and you could also add nano. So I just corrected a few spelling errors")
+print("So I copied the all the files from my thumb drive to this Project Folder and I added a .gitignore file which includes all .txt files and .pdf files. I do not expect these to be sent to Repo")
