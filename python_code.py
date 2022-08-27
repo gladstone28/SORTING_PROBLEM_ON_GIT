ad8e59ad661664c3d1672bcc1caa9eec5fd9c614
Gladstone Rose <gladstonerose@hotmail.com>
2022-08-27 02:32:30 -0500

    made changes in file on thumb drive and added to project-not recognise so I made note in python file

--- python_code.py
+++ python_code.py
@@ -1,3 +1,4 @@
 print("This is Gladstone setting up a Python Code in nano")
 print("This is another print statement in my Python code which I am using to test my understanding of git/github and you could also add nano. So I just corrected a few spelling errors")
 print("So I copied the all the files from my thumb drive to this Project Folder and I added a .gitignore file which includes all .txt files and .pdf files. I do not expect these to be sent to Repo")
+print("while in the thumb drive add a new folder with files then copied to this Project. I notice that the changes are not tracked here")
