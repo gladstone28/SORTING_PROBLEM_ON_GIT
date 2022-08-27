21a45dc57184f3bda324489e52119b37c0cc2562
Gladstone Rose <gladstonerose@hotmail.com>
2022-08-27 00:43:09 -0500

    corrected spelling error in my python code python_code.py and made notes in rose_notes.txt

--- python_code.py
+++ python_code.py
@@ -1,2 +1,2 @@
 print("This is Gladstone setting up a Python Code in nano")
-print("This is another pring statement in my Python code which I an suinig to test my understanding of git/github and you could also add nano")
+print("This is another print statement in my Python code which I am using to test my understanding of git/github and you could also add nano. So I just corrected a few spelling errors")
