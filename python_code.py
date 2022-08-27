6ce75122cf6d68af80f7bb676df9d23f7d0b6a15
Gladstone Rose <gladstonerose@hotmail.com>
2022-08-27 00:28:21 -0500

    make changes to both python_code.py and rose_notes.txt tracking my knowledge

--- python_code.py
+++ python_code.py
@@ -1,1 +1,2 @@
 print("This is Gladstone setting up a Python Code in nano")
+print("This is another pring statement in my Python code which I an suinig to test my understanding of git/github and you could also add nano")
